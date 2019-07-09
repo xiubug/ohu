621fcbc5c031457826a2f8dde678e492387df71d
魏昌华 <weichanghua@linctex.com>
2019-07-09 19:05:02 +0800

    :bug: ts

--- packages/ohu-scripts/lib/tasks/index.js
+++ packages/ohu-scripts/lib/tasks/index.js
@@ -1,1 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./clean");
+require("./typing");
+require("./doc");
+require("./dev");
+require("./build");
+require("./default");
+require("./publish-doc");
